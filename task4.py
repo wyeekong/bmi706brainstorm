b883d4d135ebf0d45b9a8c0c0520906a9b71a91e
wyeekong <wkong@bidmc.harvard.edu>
2024-03-03 11:36:08 -0500

    Update task4.py

--- task4.py
+++ task4.py
@@ -166,7 +166,7 @@ if selected_theme == "Country":
         funding=pharma2_filtered_by_phase.groupby(['source']).size().reset_index(name='count')
         funding_sorted = funding.sort_values(by='count', ascending=False)
         top_10_funding = funding_sorted.head(10)
-        pie_chart = alt.Chart(top_10_funding).mark_arc().encode(
+        pie_chart = alt.Chart(top_10_funding).mark_arc(stroke='transparent').encode(
             theta=alt.Theta(field="count", type="quantitative"),
             color=alt.Color(field="source", type="nominal"),
             tooltip=['source', 'count']
@@ -184,6 +184,8 @@ if selected_theme == "Country":
             tooltip=['source', 'year', 'count']
         ).transform_filter(
             pharma_selection
+        ).configure_axis(
+            gridOpacity=0
         ).interactive()
 
         # Display the combined chart
