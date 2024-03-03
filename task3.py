ccda1b7428ee8974e2686db77f33657a08baf66d
wyeekong <wkong@bidmc.harvard.edu>
2024-03-02 22:51:49 -0500

    new

--- task3.py
+++ task3.py
@@ -11,82 +11,90 @@ country_df = pd.read_csv('https://raw.githubusercontent.com/hms-dbmi/bmi706-2022
 df=pd.read_csv('https://raw.githubusercontent.com/wyeekong/bmi706brainstorm/main/country.csv')
 df['totaltrials'] = df.groupby(['Study population', 'year', 'phase'])['Study population'].transform('count')
 merged_df = pd.merge(df, country_df[['Country', 'country-code']], left_on='Study population',right_on='Country', how='left')
-
+merged_df = merged_df.dropna()
+merged_df['year'] = merged_df['year'].astype(int)
 
 st.set_page_config(layout="wide")
 
 # Streamlit app layout
 st.title('Antiseizure Clinical Trials Dashboard')
 
-year = st.selectbox('Select Year', options=merged_df['year'].unique())
-df_filtered = merged_df[merged_df['year'] == year]
+year = st.slider('Select Year', min_value=min(merged_df['year']), max_value=max(merged_df['year']), value=(min(merged_df['year']), max(merged_df['year'])))
+df_filtered = merged_df[merged_df['year'].between(year[0], year[1])]
 
 selected_phases = st.multiselect('Select Phase(s)', options=df_filtered['phase'].unique(), default=df_filtered['phase'].unique())
 df_filtered_by_phase = df_filtered[df_filtered['phase'].isin(selected_phases)]
 
 # Create columns for layout
-left_column, center_column, right_column = st.columns([2, 10, 5])
+left_column, right_column = st.columns([2, 15])
 
 with left_column:
     # Country Ranking List
     st.subheader('Country Ranking List')
     country_rank = df_filtered_by_phase.groupby('Study population')['totaltrials'].sum().reset_index().sort_values('totaltrials', ascending=False)
     for _, row in country_rank.iterrows():
-        st.write(f"{row['Study population']}: {row['totaltrials']} trials")
+        st.write(f"{row['Study population']}: {row['totaltrials']}")
 
   
 
-with center_column:
+with right_column:
     # Geospatial Chart
     st.subheader('Geospatial Chart')
     # Vega_datasets world data
     source = alt.topo_feature(data.world_110m.url, 'countries')
+    width = 1600
+    height  = 800
+    project = 'equirectangular'
+
+    # a gray map using as the visualization background
+    background = alt.Chart(source
+    ).mark_geoshape(
+        fill='#aaa',
+        stroke='white'
+    ).properties(
+        width=width,
+        height=height
+    ).project(project)
     
+    aggregated_data = df_filtered_by_phase.groupby(['Study population','country-code'])['totaltrials'].sum().reset_index()
+
     selector = alt.selection_single(fields=['Study population'], on='click', empty="all", clear='dblclick')
 
-    #Base chart
-    chart_base = alt.Chart(source).properties(
-        width=600,
-        height=300
-    ).project('equirectangular').add_selection(selector)
-    
-    # Geoshape for number of trials by country
-    chart_trials = chart_base.mark_geoshape().encode(
-        color=alt.Color(field="totaltrials", type="quantitative", scale=alt.Scale(scheme='oranges')),
-        tooltip=['Study population:N', 'totaltrials:Q']
+    chart_base = alt.Chart(source
+    ).properties(
+        width=width,
+        height=height
+    ).project(project
+    ).add_selection(selector
     ).transform_lookup(
         lookup="id",
-        from_=alt.LookupData(df_filtered_by_phase, "country-code", ["totaltrials", 'Study population'])
-    ).transform_filter(selector).properties(
-        title=f'Number of Trials by Country for {year} and {", ".join(selected_phases)} Phase(s)'
+        from_=alt.LookupData(aggregated_data, "country-code", ["Study population","totaltrials"]),
     )
+
     
-    # Combine the background map and trials map
-    chart2 = alt.layer(background, chart_trials).resolve_scale(color='independent')
-    
+    total_trials_sum = aggregated_data['totaltrials'].max()
+    rate_scale = alt.Scale(domain=[0, total_trials_sum], scheme='oranges')
+    rate_color = alt.Color(field="totaltrials", type="quantitative", scale=rate_scale)
+
+    chart_rate = chart_base.mark_geoshape().encode(
+        color=rate_color,
+        tooltip=['Study population:N','totaltrials:Q']
+    ).transform_filter(selector
+    ).properties(
+        title=f'Trials by county'
+    )
+
+    chart2 = alt.vconcat(background + chart_rate
+    ).resolve_scale(
+    color='independent'
+    )
+
     st.altair_chart(chart2)
 
-
-
-
-      # Heatmap of trials
-    st.subheader('Trials Heatmap')
-    # Aggregate data for heatmap
-    heatmap_data = df_filtered_by_phase.groupby(['Study population', 'year'])['totaltrials'].sum().reset_index()
-    heatmap = alt.Chart(heatmap_data).mark_rect().encode(
-        x='year:O',
-        y='Study population:N',
-        color='totltrials:Q',
-        tooltip=['Study population', 'year', 'totaltrials']
-    ).properties(
-        width=300,
-        height=300
-    )
-    st.altair_chart(heatmap, use_container_width=True)
-
-with right_column:
     # Country Selector
-    country = st.selectbox('Select Country', options=df['Study population'].unique())
+    country_trials = df_filtered_by_phase.groupby('Study population')['totaltrials'].sum()
+    top_countries = country_trials.sort_values(ascending=False).index[:20]
+    country = st.selectbox('Select Country', options=top_countries)
     df_country = df_filtered_by_phase[df_filtered_by_phase['Study population'] == country]
 
     # Line and Dot Graph for the selected country
@@ -97,7 +105,8 @@ with right_column:
         color='phase',
         tooltip=['year', 'phase', 'totaltrials']
     ).properties(
-        width=500,
-        height=300
-    ).interactive()
+        width=800,
+        height=600
+    )#.interactive()
+
     st.altair_chart(line_chart, use_container_width=True)
